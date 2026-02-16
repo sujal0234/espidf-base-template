39da47525332f87eeffb8bb06062d6e93433c699
justcucumber <sujal.malapure@gmail.com>
2026-02-17 00:44:41 +0530

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+
+
+
+void app_main(void)
+{
+
+    while(1){
+
+        
+    }
+
+}
